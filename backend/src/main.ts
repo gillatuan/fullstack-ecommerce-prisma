d06d58313857c297115d86f70c678093973052d4
TuanBDN <tuan.buidoanngoc@gmail.com>
2026-01-23 12:28:29 +0700

    config use global prefix and version

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: ['1', '2'], //v1, v2
+  });
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();

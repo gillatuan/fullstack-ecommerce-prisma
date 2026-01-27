bf688f7887373b1e66faf9379ed217cece44b1fa
gillatuan <tuan.buidoanngoc@gmail.com>
2026-01-27 22:32:29 +0700

    config to write UNIT TEST

---
+++ backend/jest.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^const/(.*)$': '<rootDir>/const/$1',
+  },
+
+  collectCoverageFrom: ['src/**/*.(t|j)s'],
+  coverageDirectory: './coverage',
+  testEnvironment: 'node',
+};
+
+export default config;
